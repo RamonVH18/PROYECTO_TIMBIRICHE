fd0c9b8b1f799dccea89aa80ad0004ed87ebff27
xime-gif <rosalesximena2813@gmail.com>
2025-11-12 09:49:00 -0700

    Interfaces modelo de juego
    - Receptores de paquetes

---
+++ ModeloJuego/src/main/java/interfaces/ReceptorPaquetesInjector.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author Ximena
+ */
+public interface ReceptorPaquetesInjector {
+    
+    public void inyectarManejador(IReceptorPaquetes receptor);
+    
+}
